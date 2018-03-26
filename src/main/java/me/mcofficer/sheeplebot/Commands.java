dccb4b54f3905d5f81b3a3acbb0c1daa4c42c4c5
M*C*O <mcofficer@gmx.de>
2018-03-26 12:49:45 +0200

    refactored JoinCommands to Commands, added new status command

--- src/main/java/me/mcofficer/sheeplebot/Commands.java
+++ src/main/java/me/mcofficer/sheeplebot/Commands.java
@@ -6,30 +6,32 @@ import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.entities.*;
 
 import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
 import java.util.Properties;
 
-public class JoinCommands implements CommandExecutor {
+public class Commands implements CommandExecutor {
 
     private String redirectUri;
     private final String clientId;
-    private Properties properties;
+    private final StatusClient statusClient;
     private final boolean enableYT;
     private final boolean enableTwitter;
+    private final Properties properties;
 
-    public JoinCommands (Bot bot) {
-        this.properties = bot.getProperties();
+    Commands (Properties properties, StatusClient statusClient) {
         try {
-           this.redirectUri = java.net.URLEncoder.encode(properties.getProperty("redirectUri"), "ISO-8859-1");
+            this.redirectUri = java.net.URLEncoder.encode(properties.getProperty("redirectUri"), "ISO-8859-1");
         }
         catch (UnsupportedEncodingException e) {
             System.out.println("Failed to encode redirect URL. Exiting.");
             System.exit(3);
         }
+        this.properties = properties;
         this.clientId = properties.getProperty("clientId");
         this.enableYT = properties.getProperty("enableYT").equals("1");
         this.enableTwitter = properties.getProperty("enableTwitter").equals("1");
+        this.statusClient = statusClient;
     }
+
 
     @Command(aliases = {"^join"}, description = "join", privateMessages = false)
     public void onJoinCommand(Guild guild, MessageChannel channel, User author, Message msg) {
@@ -53,6 +55,17 @@ public class JoinCommands implements CommandExecutor {
     }
 
 
+    @Command(aliases = {"^leave"}, description = "leave", privateMessages = false)
+    public void onLeaveCommand(Guild guild, MessageChannel channel, User author, Message msg) {
+        if (channel.getIdLong() != (Long.parseLong(properties.getProperty("channelId"))))
+            return;
+        String args = msg.getContentStripped().replace("^leave", "").trim();
+        Role role = guild.getRolesByName(args, true).get(0);
+        Member member = guild.getMember(author);
+        guild.getController().removeSingleRoleFromMember(member, role).queue(success -> msg.addReaction("👌").queue());
+    }
+
+
     private void provideOAuthLink(MessageChannel channel, User author, String service) {
         String hash = org.apache.commons.codec.digest.DigestUtils.sha1Hex(author.getId());
         String authUrl = "https://discordapp.com/oauth2/authorize?client_id=" + clientId + "&redirect_uri=" +
@@ -66,13 +79,27 @@ public class JoinCommands implements CommandExecutor {
     }
 
 
-    @Command(aliases = {"^leave"}, description = "leave", privateMessages = false)
-    public void onLeaveCommand(Guild guild, MessageChannel channel, User author, Message msg) {
-        if (channel.getIdLong() != (Long.parseLong(properties.getProperty("channelId"))))
-            return;
-        String args = msg.getContentStripped().replace("^leave", "").trim();
-        Role role = guild.getRolesByName(args, true).get(0);
-        Member member = guild.getMember(author);
-        guild.getController().removeSingleRoleFromMember(member, role).queue(success -> msg.addReaction("👌").queue());
+    @Command(aliases = {"^status"}, description = "", usage = "", privateMessages = false)
+    public void onStatusCommand(MessageChannel channel) {
+        EmbedBuilder eb = new EmbedBuilder()
+                .setTitle("SheepIt Status", "https://www.sheepit-renderfarm.com/stats.php")
+                .setThumbnail("https://s7.postimg.org/9six563ej/23316737_1679279608814045_7514368565221374242_n.png");
+        if(statusClient.checked) {
+            String desc = (statusClient.overload ?
+                    "The Server is overloaded and has entered Security Mode.\n\n" : "Everything is working fine.\n\n") +
+                    String.format(
+                    "**Frames Remaining:** %s\n" +
+                    "**Connected Clients:** %s\n" +
+                    "**Frames Rendering:** %s\n" +
+                    "**Active Projects:** %s\n",
+                    statusClient.framesRemaining, statusClient.connectedClients,
+                    statusClient.framesRendering, statusClient.activeProjects);
+            eb.setDescription(desc)
+                    .setTimestamp(statusClient.lastChecked);
+        }
+        else {
+            eb.setDescription("Status couldn't be fetched yet.");
+        }
+        channel.sendMessage(eb.build()).queue();
     }
 }
